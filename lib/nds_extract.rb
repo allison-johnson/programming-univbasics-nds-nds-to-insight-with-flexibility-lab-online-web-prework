b2482fe107cd07658a21d6227cffcb8aa442433c
Allison Johnson <allison.cuttler@gmail.com>
2019-10-15 01:11:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -81,6 +81,7 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  movies_array_with_dir = []
 end
 
 # ----------------    End of Your Code Region --------------------
